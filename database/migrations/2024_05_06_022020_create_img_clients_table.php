66dfb71945db35a47771f228eb1b7599e00dbb09
aditlfp <aditlfp@mx23>
2024-05-06 09:48:01 +0700

    Update: Post, CLient Image, Galerry, Controller, Routes, Migration,Middleware Admin, Request, Models, Folder Admin

--- database/migrations/2024_05_06_022020_create_img_clients_table.php
+++ database/migrations/2024_05_06_022020_create_img_clients_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('img_clients', function (Blueprint $table) {
             $table->id();
+            $table->string('img');
             $table->timestamps();
         });
     }

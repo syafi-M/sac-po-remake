143ff5004ba457b4d9a7c3db996ac6831f3156dc
syafi-M <syafimq00@gmail.com>
2025-05-26 09:37:03 +0700

    again

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -193,9 +193,6 @@
                                 <iframe class="w-full aspect-video rounded-md" src="{{ $vid->video_path }}" 
                                     loading="lazy"
                                     title="{{ $vid->title }}" 
-                                    frameborder="0" 
-                                    sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-presentation"
-                                    allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                     allowfullscreen>
                                 </iframe>
                             </div>
